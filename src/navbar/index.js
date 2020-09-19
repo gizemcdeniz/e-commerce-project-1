fc256d4cd36c6ca0abc951ff103f0602400d14a7
gizemcdeniz <gizemc.deniz@gmail.com>
2020-09-19 14:38:47 +0300

    first commit

---
+++ src/navbar/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+          
+        </>
+    )
+}
+export default Navbar
--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import tshirt2 from './tshirt2.png';
+import tsirt3 from './tsirt3.png';
 
 const Product = () => {
     return (
         <>
             <h2>a product</h2>
+            <img src={tshirt2} alt="Logo" />;
+            <img src={tsirt3} alt="Logo" />;
         </>
     )
 }
