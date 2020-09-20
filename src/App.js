e310b82686680a21d2b48020085f7d43a19d410a
Cem Ezgin <cemezgn@gmail.com>
2020-09-20 17:39:30 +0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import Products from './products';
 import db from './fireStoreData.js';
+import NavbarImg from './navbar';
 
 
 function App() {
-  
+
   return (
+    <>
+    < NavbarImg />
     < Products />
+    </>
       )
   }
 
